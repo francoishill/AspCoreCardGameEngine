0d18f6d254f887a460891ff91a1bb9a0e43ae0f5
Francois Hill <francoishill11@gmail.com>
2020-01-31 19:53:13 +0200

    feat: add migration script `Add__Game__Mode`

---
+++ Api/Migrations/20200131175238_Add__Game__Mode.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspCoreCardGameEngine.Api.Migrations
+{
+    public partial class Add__Game__Mode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mode",
+                table: "Games",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mode",
+                table: "Games");
+        }
+    }
+}
